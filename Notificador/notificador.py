5960ffd083d7d9601b354f498ac9ebe3e653c8a0
orsolarte4221 <o.solarte@uniandes.edu.co>
2023-09-09 13:06:33 -0500

    Se agregan variables: falla introduccioda, id ejecucion validador al notificador

--- Notificador/notificador.py
+++ Notificador/notificador.py
@@ -5,7 +5,7 @@ celery_app= Celery(__name__, broker='redis://localhost:6379/0')
 
 @celery_app.task(name='notificar')
 # Funcion registrar log recibe los parámetros y los guarda en un archivo separado por comas (csv)
-def notificar_csv(idOferta, falloHabilidad, falloPerfil, falloCalificacion, idRecursoTI, idMotor, statusMotor):
+def notificar_csv(idEjecucionValidador,fallaIntroducida,idOferta, falloHabilidad, falloPerfil, falloCalificacion, idRecursoTI, idMotor, statusMotor):
     # Obtener la fecha y hora actual en GMT-5
     fecha_hora_actual = datetime.datetime.now()
     
@@ -18,6 +18,6 @@ def notificar_csv(idOferta, falloHabilidad, falloPerfil, falloCalificacion, idRe
             file.write(nombres_columnas + '\n')
         
         # Registrar los datos en el archivo CSV
-        file.write(f'{fecha_hora_actual},{idOferta},{falloHabilidad},{falloPerfil},{falloCalificacion},{idRecursoTI},{idMotor},{statusMotor}\n')
+        file.write(f'{idEjecucionValidador},{fecha_hora_actual},{fallaIntroducida},{idOferta},{falloHabilidad},{falloPerfil},{falloCalificacion},{idRecursoTI},{idMotor},{statusMotor}\n')
     
     return True
